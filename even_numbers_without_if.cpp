1f25670b426cc26ca1149c7f5d460f41e78cf12c
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:44:59 +0600

    Add files via upload

---
+++ even_numbers_without_if.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    for(int i = 2; i <= n; i += 2)
+    {
+        cout << i << '\n';
+    }
+
+    return 0;
+}
