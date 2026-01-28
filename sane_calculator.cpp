ff629b4031bf838ce85afcd8b822dfd261683ed9
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:35:32 +0600

    Add files via upload

---
+++ sane_calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    int result = a + b * b - b + (a / b);
+
+    cout << "Result = " << result << '\n';
+
+    return 0;
+}
