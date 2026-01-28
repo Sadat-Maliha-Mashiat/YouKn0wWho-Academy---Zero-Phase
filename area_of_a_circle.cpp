00aa084d0af6f0a52dc11ad5e5c55be2bf45ad1f
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:36:34 +0600

    Add files via upload

---
+++ area_of_a_circle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double radius;
+    double PI = 3.14;
+
+    cin >> radius;
+
+    double area = PI * radius * radius;
+
+    cout << "Area = " << area << '\n';
+
+    return 0;
+}
