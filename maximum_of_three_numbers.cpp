c7583035203537cedcf4fd100eccac9b68568512
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:39:31 +0600

    Add files via upload

---
+++ maximum_of_three_numbers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a , b, c;
+
+    cout << "Enter three numbers: " << '\n';
+    cin >> a >> b >> c;
+
+    if(a >= b && a >= c)
+    {
+        cout << a << " is the maximum number.\n";
+    }
+
+    else if(b >= a && b >= c)
+    {
+        cout << b << " is the maximum number.\n";
+    }
+
+    else
+    {
+        cout << c << " is the maximum number.\n";
+    }
+
+    return 0;
+}
