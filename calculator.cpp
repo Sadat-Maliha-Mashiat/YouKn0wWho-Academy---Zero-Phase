719709a1c4593e90d4a9de1cb86e46275f5a11d5
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:40:25 +0600

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    double num1, num2;
+
+    cout << "Enter an operator (+, -, *, /): " << '\n';
+    cin >> op;
+
+    cout << "Enter two numbers: " << '\n';
+    cin >> num1 >> num2;
+
+    if(op == '+')
+    {
+        cout << num1 << " + " << num2 << " = " << (num1 + num2) << '\n';
+    }
+
+    else if(op == '-')
+    {
+        cout << num1 << " - " << num2 << " = " << (num1 - num2) << '\n';
+    }
+
+    else if(op == '*')
+    {
+        cout << num1 << " * " << num2 << " = " << (num1 * num2) << '\n';
+    }
+
+    else if(op == '/')
+    {
+        cout << num1 << " / " << num2 << " = " << (num1 / num2) << '\n';
+    }
+
+    else
+    {
+        cout << "Invalid operator!\n";
+    }
+
+    return 0;
+}
