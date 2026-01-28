726472732ba241bae7134782954824f196426019
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:48:42 +0600

    Add files via upload

---
+++ find _the_sum (array).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int A[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        sum += A[i];
+    }
+
+    cout << "Sum of the elements of an array = " << sum << '\n';
+
+    return 0;
+}
