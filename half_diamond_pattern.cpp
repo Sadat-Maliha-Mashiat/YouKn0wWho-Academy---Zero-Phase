87b1a18200aeb8b05b86fdfadddd6e4e2a363545
Sadat Maliha Mashiat <sadatmaliha703@gmail.com>
2026-01-28 16:55:18 +0600

    Add files via upload

---
+++ half_diamond_pattern.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int line = 1; line <= n; line++)
+    {
+        int space = n - line;
+        int star = 2 * line - 1;
+
+        for(int i = 1; i <= space; i++)
+        {
+            cout << " ";
+        }
+
+        for(int i = 1; i <= star; i++)
+        {
+            cout << "*";
+        }
+
+        cout << '\n';
+    }
+
+    for(int line = n - 1; line >= 1; line--)
+    {
+        int space = n - line;
+        int star = 2 * line - 1;
+
+        for(int i = 1; i <= space; i++)
+        {
+            cout << " ";
+        }
+
+        for(int i = 1; i <= star; i++)
+        {
+            cout << "*";
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
